0db18222eb595d1c06f8152516c151d324a34f5d
graememeyer <graememeyer@hotmail.com>
2020-02-21 21:50:24 +0000

    Fixed the worker count

--- debug.ps1
+++ debug.ps1
@@ -1,3 +1,23 @@
+Clear-Host
+Get-Job | Remove-Job
+
 Import-Module .\New-WorkerPool.psm1
 
 New-WorkerPool {DoSomeWork}
+
+# Write-Host ""
+# for ($i = 1; $i -le 100; $i++ )
+# {
+#     Write-Progress -Activity "Search in Progress" -Status "$i% Complete:" -PercentComplete $i;
+#     Start-Sleep -Milliseconds 100
+# }
+# Write-Host ""
+
+
+$RemainingJobs = Get-Job | where State -eq "Running"
+While($RemainingJobs){
+    Clear-Host
+    Get-Job
+    $RemainingJobs = Get-Job | where State -eq "Running"
+    Start-Sleep -milliseconds 600
+}
